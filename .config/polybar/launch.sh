68521a0e128a755d18a6413ab6d80567b655d893
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2019-01-23 19:36:37 -0800

    Adding in a short wait to make sure polybar is stopped before restarting it

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,6 +2,9 @@
 
 killall -q polybar
 
+# Wait until the processes have stopped
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+
 for m in $(polybar --list-monitors | cut -d":" -f1); do
 	MONITOR=$m polybar --reload primary &
 done
