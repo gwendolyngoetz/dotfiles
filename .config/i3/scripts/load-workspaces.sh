96397411108179bb218e454f9dd6a66bd1d9c7ce
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2020-03-11 11:37:21 -0700

    Removing loading urls at launch

--- .config/i3/scripts/load-workspaces.sh
+++ .config/i3/scripts/load-workspaces.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
 i3-msg "workspace 1:1; append_layout ~/.config/i3/workspaces/workspace-1.json; exec --no-startup-id urxvt -cd ~; exec --no-startup-id firefox;"
 
-sleep 1s
+#sleep 1s
 
-i3-msg "workspace 2:2; append_layout ~/.config/i3/workspaces/workspace-2.json; exec --no-startup-id firefox --new-window https://youtube.com/feed/subscriptions; exec --no-startup-id firefox --new-window https://messaging.gwendolyngoetz.com; exec --no-startup-id urxvt -cd ~;"
+i3-msg "workspace 2:2; append_layout ~/.config/i3/workspaces/workspace-2.json; exec --no-startup-id firefox; exec --no-startup-id firefox; exec --no-startup-id urxvt -cd ~;"
 
