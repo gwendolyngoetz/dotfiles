684be881f462b77b1f6ded4d8314790b516ac952
Gwendolyn Goetz <{ID}+{username}@users.noreply.github.com>
2020-09-09 10:14:06 -0700

    Updating audio sink defaults

--- scripts/set-default-audio-sink.sh
+++ scripts/set-default-audio-sink.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-pacmd set-default-sink "alsa_output.pci-0000_0e_00.3.analog-stereo"
-#pacmd list-sinks | grep -i alsa 
+pacmd set-default-sink "alsa_output.pci-0000_0e_00.4.analog-stereo"
+#pacmd set-default-sink "alsa_output.pci-0000_0f_00.4.analog-stereo"
+#pacmd list-sinks | grep -i "name: <alsa"
