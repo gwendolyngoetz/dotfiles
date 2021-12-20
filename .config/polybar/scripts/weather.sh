d6894b43a457f84bd74744feb71096d6d3fbf536
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2021-12-19 20:59:07 -0800

    Using the correct value from wttr

--- .config/polybar/scripts/weather.sh
+++ .config/polybar/scripts/weather.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Print the current temperature
-OUTPUT=$(curl -s wttr.in/seattle?format=j1 | jq -r '.current_condition[0].FeelsLikeF')
+#OUTPUT=$(curl -s wttr.in/seattle?format=j1 | jq -r '.current_condition[0].temp_F')
+#echo $OUTPUT°F
 
-echo $OUTPUT°F
+OUTPUT=$(curl -s wttr.in/seattle?format="%t")
+echo $OUTPUT | sed 's/+//g'
