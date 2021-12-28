342a9bff2b3b8f00e74f6d002823503a1283e5f8
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2021-12-28 09:35:57 -0800

    Updating neovim config

--- .config/nvim/init.vim
+++
@@ -1,8 +0,0 @@
-runtime! packages.vim
-runtime! mappings.vim
-runtime! settings.vim
-runtime! autocmds.vim
-
-" Plugin Settings
-lua require'config/colorizer'
-lua require'config/nvim-tree'
---
+++ .config/nvim/lua/user/keymaps.lua
@@ -0,0 +1,37 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+keymap("n", "<Up>", "<Nop>", opts)
+keymap("n", "<Down>", "<Nop>", opts)
+keymap("n", "<Left>", "<Nop>", opts)
+keymap("n", "<Left>", "<Nop>", opts)
+keymap("i", "<Up>", "<Nop>", opts)
+keymap("i", "<Down>", "<Nop>", opts)
+keymap("i", "<Left>", "<Nop>", opts)
+keymap("i", "<Left>", "<Nop>", opts)
+
+
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
