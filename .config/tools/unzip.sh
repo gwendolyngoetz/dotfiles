08e5338ed52b4695ad2c942c69c419b6d169d9d2
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2022-01-02 12:11:35 -0800

    Adding unzip script for thunar to use

---
+++ .config/tools/unzip.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Unzip into a folder the same name as the file without the .zip extension
+
+file="$1";
+dirname="$(basename $file .zip)";
+
+#echo "$file";
+#echo "$dirname";
+
+unzip "$file" -d "$dirname"
