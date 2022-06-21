10a308f3c7ffbf7cdc63073c30670dbfbd978341
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2022-06-20 17:50:50 -0700

    Adding an optional use of fd instead of find for the workon script/functions for a faster experience

--- .config/bash/workon_completions.sh
+++ .config/bash/workon_completions.sh
@@ -2,8 +2,16 @@
 #!/bin/bash
 
 function _workon_completions() {
-  COMPREPLY=($(compgen -W "$(find $HOME/src -name .git -type d -prune -exec dirname {} \; | awk -F"/" '{print $NF}' | sort)" -- "${COMP_WORDS[1]}"))
+  if [[ -f /usr/bin/fdfind ]]; then
+    COMPREPLY=($(compgen -W "$(fd -H --type directory --prune ^.git$ $HOME/src --exec-batch dirname | awk -F"/" '{print $NF}' | sort)" -- "${COMP_WORDS[1]}"))
+  else
+    COMPREPLY=($(compgen -W "$(find $HOME/src -name .git -type d -prune -exec dirname {} \; | awk -F"/" '{print $NF}' | sort)" -- "${COMP_WORDS[1]}"))
+  fi
 }
 
+
+if [[ ! -f /usr/bin/fdfind ]]; then
+  echo "fd command not found. workon completions falling back to slower find method"
+fi
 
 complete -F _workon_completions workon
