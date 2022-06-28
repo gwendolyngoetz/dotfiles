b37312cb57b239ab6d0d82cba1c1e763b306dffa
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2022-06-27 17:58:41 -0700

    Updating how i check for fd command

--- .config/bash/workon_completions.sh
+++ .config/bash/workon_completions.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 function _workon_completions() {
-  if [[ -f /usr/bin/fdfind ]]; then
+  if command -v fd &> /dev/null; then
     COMPREPLY=($(compgen -W "$(fd -H --type directory --prune ^.git$ $HOME/src --exec-batch dirname | awk -F"/" '{print $NF}' | sort)" -- "${COMP_WORDS[1]}"))
   else
     COMPREPLY=($(compgen -W "$(find $HOME/src -name .git -type d -prune -exec dirname {} \; | awk -F"/" '{print $NF}' | sort)" -- "${COMP_WORDS[1]}"))
@@ -10,7 +10,7 @@ function _workon_completions() {
 }
 
 
-if [[ ! -f /usr/bin/fdfind ]]; then
+if ! command -v fd &> /dev/null; then
   echo "fd command not found. workon completions falling back to slower find method"
 fi
 
