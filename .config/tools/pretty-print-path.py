f28dc39a339bfd98a9066df74f8a0bf5876bfb53
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2022-07-21 22:37:50 -0700

    Getting username that works on linux and mac

--- .config/tools/pretty-print-path.py
+++ .config/tools/pretty-print-path.py
@@ -1,17 +1,20 @@
 #! /usr/bin/env python3
 import os
 import pathlib
-
-color_mapping = {
-    'root':        '98;214;232',
-    os.getlogin(): '90;71;153',
-    'missing':     '255;0;0'
-}
+import pwd
 
 def get_owner(path: str):
     path_info = pathlib.Path(path)
     return path_info.owner() if path_info.exists() else 'missing'
 
+def get_username():
+    return pwd.getpwuid(os.getuid())[0]
+
+color_mapping = {
+    'root':        '98;214;232',
+    get_username(): '90;71;153',
+    'missing':     '255;0;0'
+}
 
 if __name__ == '__main__':
     for i, path in enumerate(os.environ['PATH'].split(':'), start = 1):
