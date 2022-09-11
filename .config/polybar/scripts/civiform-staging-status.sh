3808d5e9b9a7b6705b4b3efd78daafb503dbf0ae
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2022-09-11 11:29:36 -0700

    Adding right click to dismiss civ staging status. Open pavucontrol on input tab

--- .config/polybar/scripts/civiform-staging-status.sh
+++ .config/polybar/scripts/civiform-staging-status.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-TITLE="CiviForm Staging Deploy"
-MESSAGE="Passed"
-URGENCY="normal"
-SVG_RESPONSE=$(curl -s https://github.com/seattle-uat/civiform-deploy/actions/workflows/deploy-staging.yml/badge.svg?branch=main)
+SVG_RESPONSE=$(curl -s https://github.com/seattle-civiform/civiform-deploy/actions/workflows/deploy-staging.yml/badge.svg?branch=main)
 FAILING_COUNT=$(echo $SVG_RESPONSE | grep --ignore-case -o failing | wc -l)
 
 if [[ $FAILING_COUNT > 0 ]]; then
