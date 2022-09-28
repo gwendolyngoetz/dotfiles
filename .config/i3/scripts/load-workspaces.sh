5b1cf410148b140bf09293d9a41515ff336412a2
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2022-09-27 17:48:27 -0700

    Adding new workspaces

--- .config/i3/scripts/load-workspaces.sh
+++ .config/i3/scripts/load-workspaces.sh
@@ -1,9 +1,4 @@
 #!/bin/sh 
-i3-msg "workspace 1:1; append_layout ~/.config/i3/workspaces/workspace-1.json; exec --no-startup-id alacritty --working-directory ~; exec --no-startup-id firefox;"
 
-#sleep 1s
-
-i3-msg "workspace 2:2; append_layout ~/.config/i3/workspaces/workspace-2.json; exec --no-startup-id firefox; exec --no-startup-id firefox; exec --no-startup-id alacritty --working-directory ~;"
-
-i3-msg "workspace 8:8; append_layout ~/.config/i3/workspaces/workspace-8.json; exec --no-startup-id alacritty --working-directory ~;"
+~/.config/i3/scripts/load-workspaces.py 1 2 8
 
