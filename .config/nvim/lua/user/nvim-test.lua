4978fb02f01285963e1074a0610f6c30da62ba27
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2022-10-06 19:00:58 -0700

    Setting nvim-test to use toggleterm

--- .config/nvim/lua/user/nvim-test.lua
+++ .config/nvim/lua/user/nvim-test.lua
@@ -3,4 +3,11 @@ if not status_ok then
   return
 end
 
-nvim_test.setup()
+nvim_test.setup {
+  term = "toggleterm",
+  termOpts = {
+    direction = "horizontal",
+    width = 40,
+    height = 15,
+  }
+}
