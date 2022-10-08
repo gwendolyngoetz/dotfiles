240222c77766eba4cce187176179dfa347a02ef2
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2022-10-08 10:25:19 -0700

    Get netcoredbg path location dynamically

--- .config/nvim/lua/helpers.lua
+++ .config/nvim/lua/helpers.lua
@@ -11,4 +11,19 @@ M.require = function(modname)
   return module
 end
 
+M.get_from_data_path = function(part)
+  local path = vim.fn.stdpath "data" .. part
+
+  if vim.fn.empty(vim.fn.glob(path)) > 0 then
+    vim.notify("Missing: " .. path)
+    return ""
+  end
+
+  return path
+end
+
+M.is_empty = function (value)
+  return value == nil or value == ""
+end
+
 return M
