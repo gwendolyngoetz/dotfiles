be9e1369de0a245ca44f871ccb999b644c5a0882
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2022-10-08 20:23:41 -0700

    fmt

--- .config/tools/pretty-print-path.py
+++ .config/tools/pretty-print-path.py
@@ -3,17 +3,21 @@ import os
 import pathlib
 import pwd
 
+
 def get_owner(path: str):
     path_info = pathlib.Path(path)
     return path_info.owner() if path_info.exists() else 'missing'
 
+
 def get_username():
     return pwd.getpwuid(os.getuid())[0]
+
 
 def hex_to_rgb(hex_color):
     hex_color = hex_color.replace('#', '')
     rgb = tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
     return '{};{};{}'.format(str(rgb[0]), str(rgb[1]), str(rgb[2]))
+
 
 color_mapping = {
     'root':         hex_to_rgb('#62d6e8'),
@@ -22,7 +26,7 @@ color_mapping = {
 }
 
 if __name__ == '__main__':
-    for i, path in enumerate(os.environ['PATH'].split(':'), start = 1):
+    for i, path in enumerate(os.environ['PATH'].split(':'), start=1):
         owner = get_owner(path)
         color = color_mapping[owner]
 
