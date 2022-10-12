c260bccc0df38d913d788e97c2709bf56686b313
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2022-10-12 16:39:52 -0700

    Changing winbar file name fg color

--- .config/nvim/lua/user/winbar.lua
+++ .config/nvim/lua/user/winbar.lua
@@ -62,7 +62,7 @@ local get_filename = function()
       file_icon_color = ""
     end
 
-    return " " .. "%#" .. hl_group .. "#" .. file_icon .. "%*" .. " " .. "%#LineNr#" .. filename .. "%*"
+    return " " .. "%#" .. hl_group .. "#" .. file_icon .. "%*" .. " " .. "%#NavicText#" .. filename .. "%*"
   end
 end
 
