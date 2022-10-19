af3465410471bc3d83572b00e526d5c4907c5c1b
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2022-10-18 17:04:18 -0700

    Commenting out the autocmd that autohides nvim-tree. It is closing the app if you open a file from telescope fuzzy grep when you have no other files open. fmt

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -2,10 +2,10 @@
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "qf", "help", "man", "lspinfo", "spectre_panel", "lir" },
   callback = function()
-    vim.cmd [[
-      nnoremap <silent> <buffer> q :close<CR> 
-      set nobuflisted 
-    ]]
+    vim.cmd([[
+      nnoremap <silent> <buffer> q :close<CR>
+      set nobuflisted
+    ]])
   end,
 })
 
@@ -18,19 +18,19 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   end,
 })
 
-vim.cmd "autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif"
+--vim.cmd("autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif")
 
 -- Fixes Autocomment
 vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
   callback = function()
-    vim.cmd "set formatoptions-=cro"
+    vim.cmd("set formatoptions-=cro")
   end,
 })
 
 -- Highlight Yanked Text
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
   callback = function()
-    vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
+    vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
   end,
 })
 
