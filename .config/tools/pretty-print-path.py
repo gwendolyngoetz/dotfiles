9919e3802b4addbd19bf5d751ad18e647b2980ae
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2022-11-10 14:15:14 -0800

    Cleaning up pretty print path

--- .config/tools/pretty-print-path.py
+++ .config/tools/pretty-print-path.py
@@ -4,33 +4,36 @@ import pathlib
 import pwd
 
 
-def get_owner(path: str):
-    path_info = pathlib.Path(path)
-    return path_info.owner() if path_info.exists() else 'missing'
+class PathInfo:
+    def __init__(self):
+        self.color_mapping = {
+            "root": self.hex_to_rgb("#62d6e8"),
+            self.get_username(): self.hex_to_rgb("#5a4799"),
+            "missing": self.hex_to_rgb("#ff0000"),
+        }
+
+    def get_owner(self, path: str):
+        path_info = pathlib.Path(path)
+        return path_info.owner() if path_info.exists() else "missing"
+
+    def get_username(self):
+        return pwd.getpwuid(os.getuid())[0]
+
+    def hex_to_rgb(self, hex_color):
+        hex_color = hex_color.replace("#", "")
+        rgb = tuple(int(hex_color[i : i + 2], 16) for i in (0, 2, 4))
+        return "{};{};{}".format(str(rgb[0]), str(rgb[1]), str(rgb[2]))
+
+    def run(self):
+        for i, path in enumerate(os.environ["PATH"].split(":"), start=1):
+            owner = self.get_owner(path)
+            color = self.color_mapping[owner]
+
+            if owner == "missing":
+                path += " => ***no_such_directory***"
+
+            print("\033[0m{}\t\033[1;38;2;{}m{}".format(i, color, path))
 
 
-def get_username():
-    return pwd.getpwuid(os.getuid())[0]
-
-
-def hex_to_rgb(hex_color):
-    hex_color = hex_color.replace('#', '')
-    rgb = tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
-    return '{};{};{}'.format(str(rgb[0]), str(rgb[1]), str(rgb[2]))
-
-
-color_mapping = {
-    'root':         hex_to_rgb('#62d6e8'),
-    get_username(): hex_to_rgb('#5a4799'),
-    'missing':      hex_to_rgb('#ff0000')
-}
-
-if __name__ == '__main__':
-    for i, path in enumerate(os.environ['PATH'].split(':'), start=1):
-        owner = get_owner(path)
-        color = color_mapping[owner]
-
-        if owner == 'missing':
-            path += ' => ***no_such_directory***'
-
-        print('\033[0m{}\t\033[1;38;2;{}m{}'.format(i, color, path))
+if __name__ == "__main__":
+    PathInfo().run()
