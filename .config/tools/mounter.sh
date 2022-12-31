f480e18f26340f83b5c66ef88d55a7251e14c492
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2022-12-30 23:00:00 -0800

    Updating mounter script to use gum library

--- .config/tools/mounter.sh
+++ .config/tools/mounter.sh
@@ -1,24 +1,51 @@
 #!/bin/bash
 
-share="$1"
-server="truenas"
-local_path="/mnt/nfs/${server}/${share}"
-remote_path="${server}:/mnt/storage0/${share}"
-green="\e[38;2;5;225;5m"
-blue="\e[38;2;5;125;225m"
-reset="\033[0m"
+DEFAULT_SERVER="truenas"
+DEFAULT_SHARE="Documents"
 
-if [[ -z "${share}" ]]; then
-  echo "Missing share name. Try:"
-  echo ""
-  exa --tree --icons /mnt/nfs
-  exit 1
-fi
+COLOR_GREEN="\e[38;2;5;225;5m"
+COLOR_BLUE="\e[38;2;5;125;225m"
+COLOR_RESET="\033[0m"
 
-if grep -qs "${local_path} " /proc/mounts; then
-  sudo umount ${local_path}
-  printf "${blue}umounted${reset}: ${local_path}\n"
-else
-  sudo mount -t nfs "${remote_path}" "${local_path}"
-  printf "${green}mounted${reset}: ${local_path}\n"
-fi
+function run_mount {
+    echo "Server:"
+    SERVER=$(gum choose --selected=${DEFAULT_SERVER} $(ls /mnt/nfs))
+    echo "Share:"
+    SHARES=($(gum choose --no-limit --selected=${DEFAULT_SHARE} $(ls /mnt/nfs/${SERVER})))
+    for i in ${!SHARES[@]}; do
+        SHARE="${SHARES[$i]}"
+        LOCAL_PATH="/mnt/nfs/${SERVER}/${SHARE}"
+        REMOTE_PATH="${SERVER}:/mnt/storage0/${SHARE}"
+        sudo mount -t nfs "${REMOTE_PATH}" "${LOCAL_PATH}"
+        printf "${COLOR_GREEN}mounted${COLOR_RESET}: ${LOCAL_PATH}\n"
+    done
+}
+
+function run_unmount {
+    echo "Share:"
+    TO_UNMOUNT=($(gum choose --no-limit $(grep "/mnt/nfs" /proc/mounts | cut -d' ' -f2)))
+    for i in ${!TO_UNMOUNT[@]}; do
+        sudo umount ${TO_UNMOUNT[$i]}
+        printf "${COLOR_BLUE}umounted${COLOR_RESET}: ${TO_UNMOUNT[$i]}\n"
+    done
+}
+
+function run_default {
+    grep "/mnt/nfs" /proc/mounts | cut -d' ' -f2
+}
+
+function main {
+    echo "Action:"
+
+    case "$(gum choose --selected=mount mount unmount show)" in
+        "mount")
+            run_mount;;
+        "unmount")
+            run_unmount;;
+        *)
+            run_default;;
+    esac
+}
+
+
+main
