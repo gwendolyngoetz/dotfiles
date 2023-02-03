0263f1b47c47b3af8b835d4b962443916a42197e
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2023-02-02 18:34:02 -0800

    Updating script to search for specific docker containers and remove them

--- .config/docker/scripts/docker-rm-all.sh
+++ .config/docker/scripts/docker-rm-all.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-docker rm -f $(docker ps -a -q)
+SEARCH="${1}"
+
+echo "Removing these containers:" 
+
+if [[ -n "${SEARCH}" ]]; then
+    docker ps --format "{{.Names}}" | grep "${SEARCH}"
+    docker rm -f $(docker ps --format "{{.Names}} {{.ID}}" | grep "${SEARCH}" | cut -d" " -f2)
+else
+    docker ps --format "{{.Names}}"
+    docker rm -f $(docker ps -a -q)
+fi
+
