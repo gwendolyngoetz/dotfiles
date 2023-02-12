d4f19712bb02951769a896c68ba883671bddcad1
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2023-02-11 18:39:28 -0800

    Make sure filtered docker list includes stopped containers

--- .config/docker/scripts/docker-rm-all.sh
+++ .config/docker/scripts/docker-rm-all.sh
@@ -5,10 +5,10 @@ SEARCH="${1}"
 echo "Removing these containers:" 
 
 if [[ -n "${SEARCH}" ]]; then
-    docker ps --format "{{.Names}}" | grep "${SEARCH}"
-    docker rm -f $(docker ps --format "{{.Names}} {{.ID}}" | grep "${SEARCH}" | cut -d" " -f2)
+    docker ps --all --format "{{.Names}}" | grep "${SEARCH}"
+    docker rm -f $(docker ps --all --format "{{.Names}} {{.ID}}" | grep "${SEARCH}" | cut -d" " -f2)
 else
-    docker ps --format "{{.Names}}"
-    docker rm -f $(docker ps -a -q)
+    docker ps --all --format "{{.Names}}"
+    docker rm -f $(docker ps -q)
 fi
 
