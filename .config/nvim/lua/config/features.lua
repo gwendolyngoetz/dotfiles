aa21ccab9495d5f04e6fe46b81579b2fa8261a83
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2023-03-18 10:29:25 -0700

    Adding features file

---
+++ .config/nvim/lua/config/features.lua
@@ -0,0 +1,42 @@
+return {
+  dashboard = {
+    alpha = false,
+    dashboard = false,
+  },
+  lsp = {},
+  testing = {
+    neotest = false,
+    nvim_test = true,
+  },
+  tree = {
+    neotree = true,
+    nvimtree = false,
+  },
+  autopairs = true,
+  autotag = true,
+  bufferline = false, -- Tabs at top, using winbar instead
+  cmp = true,
+  colorful_winsep = false, -- Stopped using because redraw flickers
+  colorizer = true,
+  comment = true,
+  dap = true,
+  eyeliner = true,
+  gitsigns = true,
+  illuminate = true,
+  indentline = true,
+  java = true,
+  lualine = true,
+  mason = true,
+  metals = true,
+  notify = false, --true
+  nvim_lightbulb = false, -- Not currently using or setup
+  nvim_scrollbar = true,
+  pretty_fold = true,
+  project = true,
+  telescope = true,
+  toggleterm = false, -- Not currently using
+  treesitter = true,
+  winbar = true,
+  whichkey = true,
+  worktree = true,
+}
