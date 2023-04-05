2b3fda7ba0db3b5c2542c0e0b1b5ed21632fdc95
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2023-04-04 21:56:42 -0700

    Enabling new neovim loader feature

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,7 @@
+-- Currently this is a opt in. Watch for later
+-- when this is default and can be removed
+vim.loader.enable()
+
 require("config.options")
 require("config.plugin-bootstrap")
 require("config.keymaps")
