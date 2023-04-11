88f4728d7193a300ad4bc2a84fce51461eb57986
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2023-04-10 20:10:23 -0700

    Cleaning up workon completions

--- .config/bash/workon_completions.sh
+++ .config/bash/workon_completions.sh
@@ -1,17 +1,13 @@
-# vim: ft=bash
 #!/bin/bash
 
 function _workon_completions() {
-  if command -v fd &> /dev/null; then
     COMPREPLY=($(compgen -W "$(fd -H --type directory --prune ^.git$ $HOME/src --exec-batch dirname | awk -F"/" '{print $NF}' | sort)" -- "${COMP_WORDS[1]}"))
-  else
-    COMPREPLY=($(compgen -W "$(find $HOME/src -name .git -type d -prune -exec dirname {} \; | awk -F"/" '{print $NF}' | sort)" -- "${COMP_WORDS[1]}"))
-  fi
 }
 
 
 if ! command -v fd &> /dev/null; then
-  echo "fd command not found. workon completions falling back to slower find method"
+  echo "fd command not found"
+  return
 fi
 
 complete -F _workon_completions workon
