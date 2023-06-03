ca57ef502763fde26ac8d8dea96f3c8650bddd9a
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2023-06-03 14:25:56 -0700

    Fixing output when api call fails

--- .config/polybar/scripts/air-quality.sh
+++ .config/polybar/scripts/air-quality.sh
@@ -1,4 +1,4 @@
-#!/bin/sh 
+#!/bin/bash 
 
 
 # Set in your .profile file
@@ -6,17 +6,22 @@
 #AIRNOW_API_KEY=''     # api key from registering 
 
 
-AIRNOW_API_URL="http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=$AIRNOW_API_ZIPCODE&API_DATE&API_KEY=$AIRNOW_API_KEY" 
+AIRNOW_API_URL="http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=${AIRNOW_API_ZIPCODE}&API_DATE&API_KEY=${AIRNOW_API_KEY}" 
 
-AIRNOW_API_RESPONSE=$(curl --silent --location --request GET $AIRNOW_API_URL)
+AIRNOW_API_RESPONSE=$(curl --silent --location --request GET "${AIRNOW_API_URL}")
 
 
-OUTPUT=$(echo $AIRNOW_API_RESPONSE | jq -r '.[]
+OUTPUT=$(echo "${AIRNOW_API_RESPONSE}" | jq -r '.[]
 | select(.ParameterName=="PM2.5")
 | ((.AQI|tostring) + " " + .Category.Name)
-| .[0:5]')
+| .[0:5]' 2>&1)
 
 
-echo $OUTPUT | awk -F" " '{ print $1 "-" substr($2,1,1) }'
+if [[ "${OUTPUT:0:11}" = "parse error" ]]; then
+    echo "??"
+else
+    echo "${OUTPUT}" | awk -F" " '{ print $1 "-" substr($2,1,1) }'
+fi
+
 
 
