acea1a15053e02f114a92ab196b303bc90456084
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2023-08-11 11:24:22 -0700

    Adding filtering

--- .config/docker/scripts/docker-rm-img.sh
+++ .config/docker/scripts/docker-rm-img.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-docker rmi -f $(docker images -a -q)
+SEARCH="${1}"
+
+echo "Removing images:" 
+
+if [[ -n "${SEARCH}" ]]; then
+    docker images --all --format "{{.Names}}:{{.Tag}}" | grep "${SEARCH}" | sort
+    docker rmi -f $(docker images --all --format "{{.ID}}" | grep "${SEARCH}")
+
+else
+    docker images --all --format "{{.Names}}:{{.Tag}}" | sort
+    docker rmi -f $(docker images -a -q)
+fi
+
