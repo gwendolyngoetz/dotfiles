92f90d03b4f59b660eff4422b4496cd37d255100
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2023-08-21 16:01:16 -0700

    Disable autopairs/autotags and remove unused nvim-lightbulb config

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -1,10 +1,9 @@
 local config = function()
   require("nvim-autopairs").setup({
     check_ts = true,
-    disable_filetype = { "TelescopePrompt" },
   })
 
-  local on_confirm_done = require("nvim-autopairs.completion.cmp").on_confirm_done({})
+  local on_confirm_done = require("nvim-autopairs.completion.cmp").on_confirm_done()
   require("cmp").event:on("confirm_done", on_confirm_done)
 end
 
@@ -12,6 +11,7 @@ return {
   {
     "windwp/nvim-autopairs",
     commit = "e8f7dd7",
+    enabled = false,
     config = config,
   },
 }
--- .config/nvim/lua/plugins/autotag.lua
+++ .config/nvim/lua/plugins/autotag.lua
@@ -6,6 +6,7 @@ return {
   {
     "windwp/nvim-ts-autotag",
     commit = "6be1192",
+    enabled = false,
     config = config,
   },
 }
