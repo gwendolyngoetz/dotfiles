a96d70c61b4ec5d63684e9d39317daeeb13679f8
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2023-09-16 09:18:18 -0700

    Adding fzf-based script for selecting two git commits that displays in the difftool

---
+++ .config/git/scripts/diff-commits.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+function get_commit {
+    prompt_label="${1} Commit> "
+    commit=$(git --no-pager log \
+            --oneline \
+            --color=always \
+            --pretty="format:%C(auto)%h%Creset - %s %C(dim green)(%cr) %C(blue)[%an]" \
+        | fzf \
+            +s \
+            --ansi \
+            --info=hidden \
+            --color="bg+:-1,prompt:4,border:#5a4799,hl:4,hl+:4,fg+:6" \
+            --prompt="${prompt_label}")
+
+    echo "${commit}" | cut -d" " -f1
+}
+
+commit_newer=$(get_commit "Newer")
+if [[ -z "${commit_newer}" ]]; then
+    exit 1
+fi
+
+commit_older=$(get_commit "Older")
+
+if [[ -z "${commit_older}" ]]; then
+    exit 2
+fi
+
+echo "Comparing commit ${commit_older} and ${commit_newer}"
+
+git difftool -d "${commit_older}" "${commit_newer}"
+
+
