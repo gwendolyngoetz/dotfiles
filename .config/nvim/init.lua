cd2b215bbc50e18bb787343679fdaaf1785b352b
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2023-10-20 14:35:51 -0700

    Temporarily disable lsp file watching due to perf

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,6 +2,14 @@
 -- when this is default and can be removed
 vim.loader.enable()
 
+-- Temporarily disable lsp file watcher due to performance issues
+local ok, wf = pcall(require, "vim.lsp._watchfiles")
+if ok then
+  wf._watchfunc = function()
+    return function() end
+  end
+end
+
 require("config.options")
 require("config.plugin-bootstrap")
 require("config.keymaps")
