213486e508bed3c88ceb5e0b19024d748ec571ec
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2023-11-22 11:52:16 -0800

    fix rm script

--- .config/docker/scripts/docker-rm-img.sh
+++ .config/docker/scripts/docker-rm-img.sh
@@ -5,11 +5,10 @@ SEARCH="${1}"
 echo "Removing images:" 
 
 if [[ -n "${SEARCH}" ]]; then
-    docker images --all --format "{{.Names}}:{{.Tag}}" | grep "${SEARCH}" | sort
-    docker rmi -f $(docker images --all --format "{{.ID}}" | grep "${SEARCH}")
+    docker images --all --format "{{.Repository}}:{{.Tag}}" | grep "${SEARCH}" | sort
+    docker rmi -f $(docker images --all --format "{{.ID}}|{{.Repository}}" | grep "${SEARCH}" | cut -d'|' -f1)
 
 else
-    docker images --all --format "{{.Names}}:{{.Tag}}" | sort
-    docker rmi -f $(docker images -a -q)
+    docker images --all --format "{{.Repository}}:{{.Tag}}" | sort
+    docker rmi -f $(docker images --all -q)
 fi
-
