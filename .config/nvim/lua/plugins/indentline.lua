5d818c6608a3d4cfb9f496f7533213a1a455467e
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-03-31 09:25:40 -0700

    Nvim: Updating indent-blankline plugin from v2 to v3

--- .config/nvim/lua/plugins/indentline.lua
+++ .config/nvim/lua/plugins/indentline.lua
@@ -1,19 +1,25 @@
 local config = function()
   local icons = require("config.settings").icons
 
-  require("indent_blankline").setup({
-    show_current_context = true,
-    indent_blankline_char = icons.indent.blankline_char,
-    indent_blankline_show_trailing_blankline_indent = false,
-    indent_blankline_show_first_indent_level = true,
-    indent_blankline_use_treesitter = true,
-    indent_blankline_show_current_context = true,
-    indent_blankline_buftype_exclude = { "terminal", "nofile" },
-    indent_blankline_filetype_exclude = {
-      "help",
-      "packer",
-      "NvimTree",
-      "neo-tree",
+  require("ibl").setup({
+    indent = {
+      char = icons.indent.blankline_char,
+    },
+    exclude = {
+      filetypes = {
+        "help",
+        "NvimTree",
+        "neo-tree",
+      },
+      buftypes = {
+        "terminal",
+        "nofile",
+      },
+    },
+    scope = {
+      char = icons.indent.blankline_char,
+      show_start = false,
+      show_end = false,
     },
   })
 end
@@ -21,7 +27,7 @@ end
 return {
   {
     "lukas-reineke/indent-blankline.nvim",
-    tag = "v2.20.8",
+    tag = "v3.5.4",
     config = config,
   },
 }
