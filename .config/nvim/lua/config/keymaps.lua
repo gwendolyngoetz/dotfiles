f1bc1937cbaf8f4b385f2a28841154303c48b43d
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-06-02 15:24:47 -0700

    Nvim: Comment out navigation keymaps to enabled vim-tmux-navigator

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -22,10 +22,10 @@ vim.keymap.set("i", "<Left>", "<Nop>")
 vim.keymap.set("i", "<Right>", "<Nop>")
 
 -- Better window navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
+--vim.keymap.set("n", "<C-h>", "<C-w>h")
+--vim.keymap.set("n", "<C-j>", "<C-w>j")
+--vim.keymap.set("n", "<C-k>", "<C-w>k")
+--vim.keymap.set("n", "<C-l>", "<C-w>l")
 
 vim.keymap.set("n", "<Left>", "<C-w>h")
 vim.keymap.set("n", "<Down>", "<C-w>j")
