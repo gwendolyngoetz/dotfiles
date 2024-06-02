d91257832d9f729884652a85dbf96aef272d3397
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-06-02 11:25:59 -0700

    Renumbering secondary screen workspaces

--- .config/i3/scripts/load-workspaces.sh
+++ .config/i3/scripts/load-workspaces.sh
@@ -1,4 +1,4 @@
 #!/bin/sh 
 
 i3-companion load --workspace=1 --output-to=DisplayPort-0
-i3-companion load --workspace=2 --output-to=DisplayPort-1
+i3-companion load --workspace=6 --output-to=DisplayPort-1
