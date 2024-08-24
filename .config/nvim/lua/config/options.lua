37d00d213c38f496862b539adc0d896782d54254
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-08-24 10:48:40 -0700

    NVIM: Update provider config

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -57,9 +57,11 @@ vim.opt.shortmess:append("c")
 vim.opt.shortmess:remove("F") -- Disable for Metals
 vim.opt.whichwrap:append("<,>,[,]")
 
-vim.cmd("let g:loaded_perl_provider = 0")
-vim.cmd("let g:loaded_ruby_provider = 0")
-vim.cmd("let g:python3_host_prog = '/usr/bin/python3'")
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
+-- vim.g.loaded_python3_provider = 0
+vim.g.python3_host_prog = '/usr/bin/python3'
 
 -- Only used below in vim.opt.foldtext
 function _G.custom_foldtext()
