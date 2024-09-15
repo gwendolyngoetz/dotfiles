e4a29312d5fd089ebb2bea203b1eeb4e3db1604c
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-09-15 12:47:26 -0700

    Nvim: Cleanup ibl module

--- .config/nvim/lua/plugins/indentline.lua
+++ .config/nvim/lua/plugins/indentline.lua
@@ -1,33 +1,29 @@
-local config = function()
-    local icons = require("config.settings").icons
-
-    require("ibl").setup({
-        indent = {
-            char = icons.indent.blankline_char,
-        },
-        exclude = {
-            filetypes = {
-                "help",
-                "NvimTree",
-                "neo-tree",
-            },
-            buftypes = {
-                "terminal",
-                "nofile",
-            },
-        },
-        scope = {
-            char = icons.indent.blankline_char,
-            show_start = false,
-            show_end = false,
-        },
-    })
-end
-
 return {
     {
         "lukas-reineke/indent-blankline.nvim",
         tag = "v3.7.2",
-        config = config,
+        main = "ibl",
+
+        opts = {
+            indent = {
+                char = require("config.settings").icons.indent.blankline_char,
+            },
+            exclude = {
+                filetypes = {
+                    "help",
+                    "NvimTree",
+                    "neo-tree",
+                },
+                buftypes = {
+                    "terminal",
+                    "nofile",
+                },
+            },
+            scope = {
+                char = require("config.settings").icons.indent.blankline_char,
+                show_start = false,
+                show_end = false,
+            },
+        }
     },
 }
