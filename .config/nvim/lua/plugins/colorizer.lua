447297f63774438337e53910832944152e0f8afb
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-09-15 12:51:15 -0700

    Nvim: Cleanup colorizer module

--- .config/nvim/lua/plugins/colorizer.lua
+++ .config/nvim/lua/plugins/colorizer.lua
@@ -1,23 +1,19 @@
-local config = function()
-    require("colorizer").setup({
-        filetypes = {
-            "css",
-            "javascript",
-            "html",
-        },
-        user_default_options = {
-            rgb_fn = true,
-            hsl_fn = true,
-        }
-    })
-end
-
 return {
     {
         "NvChad/nvim-colorizer.lua",
-        config = config,
         keys = {
             { "<C-c>", "<cmd>ColorizerToggle<CR>", desc = "Toggle Colorizer" }
+        },
+        opts = {
+            filetypes = {
+                "css",
+                "javascript",
+                "html",
+            },
+            user_default_options = {
+                rgb_fn = true,
+                hsl_fn = true,
+            }
         }
     },
 }
