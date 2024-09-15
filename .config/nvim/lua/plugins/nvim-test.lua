f0bad9ade358339b2f7c99fb21db453096d8584f
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-09-15 12:59:00 -0700

    Nvim: Cleanup nvim-test

--- .config/nvim/lua/plugins/nvim-test.lua
+++ .config/nvim/lua/plugins/nvim-test.lua
@@ -1,22 +1,18 @@
-local config = function()
-    require("nvim-test").setup({
-        term = "toggleterm",
-        termOpts = {
-            direction = "horizontal",
-            width = 40,
-            height = 15,
-        },
-    })
-end
-
 return {
     {
         "klen/nvim-test",
-        config = config,
         dependencies = {
             {
                 "akinsho/toggleterm.nvim",
             },
         },
+        opts = {
+            term = "toggleterm",
+            termOpts = {
+                direction = "horizontal",
+                width = 40,
+                height = 15,
+            }
+        }
     },
 }
