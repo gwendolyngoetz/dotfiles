431f11a12bb6a44d5394b493570311d160399ef6
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-09-14 18:09:30 -0700

    Nvim: Disable autoformat on save. Setup binding

--- .config/nvim/lua/plugins/autoformat.lua
+++ .config/nvim/lua/plugins/autoformat.lua
@@ -2,16 +2,6 @@ return { -- Autoformat
     "stevearc/conform.nvim",
     opts = {
         notify_on_error = false,
-        format_on_save = function(bufnr)
-            -- Disable "format_on_save lsp_fallback" for languages that don't
-            -- have a well standardized coding style. You can add additional
-            -- languages here or re-enable it for the disabled ones.
-            local disable_filetypes = { c = true, cpp = true }
-            return {
-                timeout_ms = 500,
-                lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-            }
-        end,
         -- https://github.com/stevearc/conform.nvim?tab=readme-ov-file#formatters
         formatters_by_ft = {
             go = { "goimports", "gofumpt" },
@@ -22,4 +12,7 @@ return { -- Autoformat
             xml = { "xmlformat" },
         },
     },
+    keys = {
+        { "<leader>F", "<cmd>lua require('conform').format()<CR>", desc = "Format" }
+    }
 }
