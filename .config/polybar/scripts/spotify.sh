ce96a7a10d3dcdb78a20dd3bc6374320e8b38940
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-11-06 18:39:33 -0800

    Polybar: Spotify script prevents showing errors

--- .config/polybar/scripts/spotify.sh
+++ .config/polybar/scripts/spotify.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # modified from https://github.com/NicholasFeldman/dotfiles/blob/master/polybar/.config/polybar/spotify.sh
 main() {
   if ! pgrep -x spotify >/dev/null; then
@@ -14,8 +14,13 @@ main() {
 
   artist=$(echo "$meta" | sed -nr '/xesam:artist"/,+2s/^ +string "(.*)"$/\1/p' | tail -1  | sed 's/\&/\\&/g' | sed 's#\/#\\/#g')
   title=$(echo "$meta" | sed -nr '/xesam:title"/,+2s/^ +variant +string "(.*)"$/\1/p' | tail -1 | sed 's/\&/\\&/g'| sed 's#\/#\\/#g')
+  result="$(echo "${*:-%title% - %artist%}" | sed "s/%artist%/$artist/g;s/%title%/$title/g"i | cut -c1-30)"
 
-  echo "${*:-%title% - %artist%}" | sed "s/%artist%/$artist/g;s/%title%/$title/g"i | cut -c1-30
+  if [[ "${result}" == *"freedesktop"* ]]; then
+    return
+  fi
+
+  echo "${result}"
 }
 
 main "$@"
