cdaf2c86dce3507c3b61cf60de697d8624db26a1
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-11-08 20:07:29 -0800

    NVIM: Adding binding to toggle markview

--- .config/nvim/lua/plugins/markview.lua
+++ .config/nvim/lua/plugins/markview.lua
@@ -8,6 +8,9 @@ return {
             -- Or if the parsers are in your $RUNTIMEPATH
             "nvim-treesitter/nvim-treesitter",
             "nvim-tree/nvim-web-devicons"
+        },
+        keys = {
+            { "<leader>mm", "<cmd>Markview toggleAll<CR>", { desc = "Toggle Markview" }},
         }
     }
 }
