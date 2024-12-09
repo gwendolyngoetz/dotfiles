2adc61fcb274dee90dde6586c8a5389532c16245
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-12-08 20:21:13 -0800

    Nvim: Updating formatting. Enable auto-format on save

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,19 +1,38 @@
+local prettier = { "prettierd", "prettier", stop_after_first = true }
+
 return {
     "stevearc/conform.nvim",
     opts = {
-        notify_on_error = false,
+        notify_on_error = true,
         -- https://github.com/stevearc/conform.nvim?tab=readme-ov-file#formatters
         formatters_by_ft = {
+            css = prettier,
             go = { "goimports", "gofumpt" },
-            javascript = { "prettierd", "prettier", stop_after_first = true },
-            json = { "prettierd", "prettier", stop_after_first = true },
+            java = { "google-java-format" },
+            javascript = prettier,
+            json = prettier,
+            html = prettier,
             lua = { "stylua --indent-type Spaces" },
+            md = prettier,
             python = { "isort", "black" },
+            sass = prettier,
+            scala = { "scalafmt" },
+            scss = prettier,
+            sql = { "sql_formatter" },
+            sh = { "shfmt" },
+            -- sh = { "shfmt", "shellcheck" },
             terraform = { "terraform_fmt" },
+            ts = prettier,
             xml = { "xmlformat" },
+            yaml = prettier,
+            yml = prettier
+        },
+        format_on_save = {
+            timeout_ms = 500,
+            lsp_format = "fallback"
         },
     },
-    keys = {
-        { "<leader>F", "<cmd>lua require('conform').format()<CR>", desc = "Format" }
-    }
+    -- keys = {
+    --     { "<leader>f", "<cmd>lua require('conform').format()<CR>", desc = "Format" }
+    -- }
 }
