4b93413370509c863ca7dbb03720c578e260907f
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2025-01-02 14:24:59 -0800

    Nvim: Refactor metals.lua

--- .config/nvim/lua/plugins/metals.lua
+++ .config/nvim/lua/plugins/metals.lua
@@ -1,23 +1,4 @@
-local config = function()
-    local metals_config = require("metals").bare_config()
-
-    -- Example of settings
-    metals_config.settings = {
-        showImplicitArguments = true,
-        excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
-    }
-
-    -- *READ THIS*
-    -- I *highly* recommend setting statusBarProvider to true, however if you do,
-    -- you *have* to have a setting to display this in your statusline or else
-    -- you'll not see any messages from metals. There is more info in the help
-    -- docs about this
-    -- metals_config.init_options.statusBarProvider = "on"
-
-    -- Example if you are using cmp how to make sure the correct capabilities for snippets are set
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-
+local setup_dap = function()
     require("dap").configurations.scala = {
         {
             type = "scala",
@@ -37,24 +18,6 @@ local config = function()
             },
         },
     }
-
-    metals_config.on_attach = function()
-        require("metals").setup_dap()
-    end
-
-    -- Autocmd that will actually be in charging of starting the whole thing
-    local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-
-    vim.api.nvim_create_autocmd("FileType", {
-        -- NOTE: You may or may not want java included here. You will need it if you
-        -- want basic Java support but it may also conflict if you are using
-        -- something like nvim-jdtls which also works on a java filetype autocmd.
-        pattern = { "scala", "sbt", "java" },
-        callback = function()
-            require("metals").initialize_or_attach(metals_config)
-        end,
-        group = nvim_metals_group,
-    })
 end
 
 return {
@@ -63,6 +26,38 @@ return {
         dependencies = {
             "nvim-lua/plenary.nvim",
         },
-        config = config,
+        ft = { "scala", "sbt", "java" },
+        opts = function()
+            local metals_config = require("metals").bare_config()
+
+            metals_config.settings = {
+                showImplicitArguments = true,
+                excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+            }
+
+            metals_config.on_attach = function()
+                require("metals").setup_dap()
+            end
+
+            return metals_config
+        end,
+        config = function(self, metals_config)
+            local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+
+            setup_dap()
+
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = self.ft,
+                callback = function()
+                    require("metals").initialize_or_attach(metals_config)
+                end,
+                group = nvim_metals_group,
+            })
+        end
+
+
     },
 }
