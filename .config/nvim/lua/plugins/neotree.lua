bbc4e5cfd09a4bc66a52c1274937310840c67102
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2025-01-12 12:39:19 -0800

    Nvim: Lazy update

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -3,7 +3,7 @@ local icons = require("config.settings").icons
 return {
     {
         "nvim-neo-tree/neo-tree.nvim",
-        tag = "3.27",
+        tag = "3.28",
         dependencies = {
             "nvim-lua/plenary.nvim",
             "nvim-tree/nvim-web-devicons",
