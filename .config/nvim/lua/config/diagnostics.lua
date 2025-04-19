fd0dc37c85757c00a4f162929c6c50a0f5fb4d80
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2025-04-18 20:27:51 -0700

    Nvim: Cleaning up deprecated diagnostics settings

--- .config/nvim/lua/config/diagnostics.lua
+++ .config/nvim/lua/config/diagnostics.lua
@@ -7,39 +7,17 @@ local diagnostic_config = {
     --     current_line = true
     -- }
     virtual_text = true,
-    signs = true,
     update_in_insert = true,
     underline = true,
     severity_sort = true,
     float = {
         focusable = false,
         style = "minimal",
-        -- border = settings.ui.border,
+        border = settings.ui.border,
         source = true,
         header = "",
         prefix = "",
     },
-}
-
-vim.diagnostic.config(diagnostic_config)
-
--- vim.lsp.handlers["textDocument/hover"] = vim.lsp.buf.with(vim.lsp.buf.hover, {
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-    border = settings.ui.border,
-})
-
--- vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.buf.with(vim.lsp.buf.signature_help, {
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-    border = settings.ui.border,
-})
-
--- setup icons
-vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { fg = "#db4b4b", bg = "#4a1717" })
-vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", { fg = "#e0af68", bg = "#4a3b21" })
-vim.api.nvim_set_hl(0, "DiagnosticVirtualTextInfo", { fg = "#0db9d7", bg = "#013d49" })
-vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", { fg = "#1abc9c", bg = "#05392e" })
-
-vim.diagnostic.config({
     signs = {
         text = {
             [vim.diagnostic.severity.ERROR] = icons.diagnostics.error,
@@ -48,4 +26,20 @@ vim.diagnostic.config({
             [vim.diagnostic.severity.HINT] = icons.diagnostics.hint
         }
     }
+}
+
+vim.diagnostic.config(diagnostic_config)
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.buf.hover({
+    border = settings.ui.border,
 })
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.buf.signature_help({
+    border = settings.ui.border
+})
+
+-- setup icons
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { fg = "#db4b4b", bg = "#4a1717" })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", { fg = "#e0af68", bg = "#4a3b21" })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextInfo", { fg = "#0db9d7", bg = "#013d49" })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", { fg = "#1abc9c", bg = "#05392e" })
