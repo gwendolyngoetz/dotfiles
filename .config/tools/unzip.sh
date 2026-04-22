eaf574ac1dda4d000ade0f9ef1cbdaa990c127ed
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2026-04-22 07:29:43 -0700

    Updating thunar unzip script for more than type zip

--- .config/tools/unzip.sh
+++ .config/tools/unzip.sh
@@ -1,11 +1,47 @@
 #!/bin/sh
 
-# Unzip into a folder the same name as the file without the .zip extension
+# Extract archive into a folder named after the file (without extension)
 
-file="$1";
-dirname="$(basename "$file" .zip)";
+file="$1"
 
-#echo "$file";
-#echo "$dirname";
-
-unzip "$file" -d "$dirname"
+case "$file" in
+*.tar.gz | *.tgz)
+    dirname="$(basename "$file" .tar.gz)"
+    [ "$dirname" = "$(basename "$file")" ] && dirname="$(basename "$file" .tgz)"
+    mkdir -p "$dirname"
+    tar xzf "$file" -C "$dirname"
+    ;;
+*.tar.bz2)
+    dirname="$(basename "$file" .tar.bz2)"
+    mkdir -p "$dirname"
+    tar xjf "$file" -C "$dirname"
+    ;;
+*.tar.xz)
+    dirname="$(basename "$file" .tar.xz)"
+    mkdir -p "$dirname"
+    tar xJf "$file" -C "$dirname"
+    ;;
+*.tar)
+    dirname="$(basename "$file" .tar)"
+    mkdir -p "$dirname"
+    tar xf "$file" -C "$dirname"
+    ;;
+*.zip)
+    dirname="$(basename "$file" .zip)"
+    unzip "$file" -d "$dirname"
+    ;;
+*.7z)
+    dirname="$(basename "$file" .7z)"
+    mkdir -p "$dirname"
+    7z x "$file" -o"$dirname"
+    ;;
+*.rar)
+    dirname="$(basename "$file" .rar)"
+    mkdir -p "$dirname"
+    unrar x "$file" "$dirname/"
+    ;;
+*)
+    echo "Unsupported format: $file" >&2
+    exit 1
+    ;;
+esac
