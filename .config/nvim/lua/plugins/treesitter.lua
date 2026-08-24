e5ffd28fa03598387c4dd60302bea1aaf9b2174e
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2026-08-24 10:04:30 -0700

    Fixing treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,58 @@
 local config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
+    require("nvim-treesitter").setup()
+
+    require("nvim-treesitter").install({
+        "bash",
+        "c_sharp",
+        "cmake",
+        "css",
+        "go",
+        "hcl",
+        "html",
+        "java",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "ocaml",
+        "python",
+        "scala",
+        "sql",
+        "vim",
+        "vimdoc",
+        "yaml",
+    })
+
+    local no_highlight = { css = true }
+    local no_indent = { python = true, css = true }
+
+    vim.api.nvim_create_autocmd("FileType", {
+        callback = function(args)
+            local lang = vim.treesitter.language.get_lang(args.match)
+            if not lang or not vim.treesitter.language.add(lang) then
+                return
+            end
+            if not no_highlight[lang] then
+                vim.treesitter.start(args.buf, lang)
+            end
+            if not no_indent[lang] then
+                vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end
+        end,
+    })
 end
 
 return {
     {
         "nvim-treesitter/nvim-treesitter-textobjects",
+        branch = "main",
         dependencies = {
             "nvim-treesitter/nvim-treesitter",
         },
+        config = function()
+            require("nvim-treesitter-textobjects").setup()
+        end,
     },
     {
         "JoosepAlviste/nvim-ts-context-commentstring",
@@ -17,47 +62,9 @@ return {
     },
     {
         "nvim-treesitter/nvim-treesitter",
-        -- lazy = false,
-        branch = "master",
+        lazy = false,
+        branch = "main",
         build = ":TSUpdate",
         config = config,
-        opts = {
-            auto_install = true,
-            sync_install = false,
-            ensure_installed = {
-                "bash",
-                "c_sharp",
-                "cmake",
-                "css",
-                "go",
-                "hcl",
-                "html",
-                "java",
-                "javascript",
-                "json",
-                "lua",
-                "python",
-                "markdown",
-                "ocaml",
-                "scala",
-                "sql",
-                "vimdoc",
-                "vim",
-                "yaml",
-            },
-            ignore_install = { "" },
-            modules = {},
-            highlight = {
-                enable = true,
-                disable = { "css" },
-            },
-            autopairs = {
-                enable = true,
-            },
-            indent = {
-                enable = true,
-                disable = { "python", "css" },
-            },
-        }
     },
 }
