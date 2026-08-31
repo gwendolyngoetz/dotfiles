040cd97d22ec8b6097556c3d1271bca2d59fffd9
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2026-08-31 13:36:21 -0700

    hyprland: migration tweaks

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -25,7 +25,13 @@ local black = "#000000"   -- solid black desktop background (i3: feh + black.jpg
 -- Monitors
 ------------------------------------------------------------
 
-hl.monitor({ output = "", mode = "preferred", position = "auto", scale = 1 })
+hl.monitor({
+    output = "Virtual-1",
+    mode = "1920x1080@60",
+    position = "0x0",
+    scale = 1,
+})
+-- hl.monitor({ output = "", mode = "preferred", position = "auto", scale = 1 })
 
 ------------------------------------------------------------
 -- General options
